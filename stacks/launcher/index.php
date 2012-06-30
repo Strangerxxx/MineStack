84cd7734b73cb3a30ecd439b055842d7a0c5af93
Stranger <black.danek@gmail.com>
2012-06-30 08:03:55 +0400

    stack update

--- stacks/launcher/index.php
+++ stacks/launcher/index.php
@@ -1,21 +1,20 @@
 <?php
 /*
-Plugin Name: Name Of The Plugin
-Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+Stack Name: Launcher
+Stack URI: http://URI_Of_Page_Describing_Plugin_and_Updates
 Description: A brief description of the Plugin.
 Version: The Plugin's Version Number, e.g.: 1.0
 Author: Name Of The Plugin Author
 Author URI: http://URI_Of_The_Plugin_Author
 License: A "Slug" license name e.g. GPL2
 */
-define( 'STACKPATH', stack_dir_path(__FILE__) );
+define( 'LAUNCHER_STACKPATH', stack_dir_path(__FILE__) );
 function getContent() {
 	global $wpdb;
-	include(STACKPATH."/pages/".$_GET['mine'].".php");
-	//return $return;
+	include(LAUNCHER_STACKPATH."/pages/".$_GET['mine'].".php");
 }
 function getTemplate() {
-	include("blank.php");
+	include(LAUNCHER_STACKPATH."/blank.php");
 	exit;
 }
 if ($_GET['mine'] == 'launcher') {
